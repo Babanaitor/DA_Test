e716e277272937a3accd1757d49b36ea2dcc2449
Babanaitor <41361365+Babanaitor@users.noreply.github.com>
2021-07-11 01:18:53 +0400

    Update app.py

--- app.py
+++ app.py
@@ -1,46 +1,20 @@
-######################################################### 1 is weekly improving?
-
-
+import dash_core_components as dcc
+import dash_html_components as html
 import pandas as pd
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 import dash
 
-app = dash.Dash(__name__)
-server = app.server
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-# open data file
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 user_data_df = pd.read_csv("User_data.csv")
-
-# check data and its type
-print((user_data_df.at[0, 'Date']))
-print(type(user_data_df.at[0, 'Date']))
-
-# covert str to datetime
 user_data_df['Date'] = pd.to_datetime(user_data_df['Date'])
-
-# check data type again
-print((user_data_df.at[0, 'Date']))
-print(type(user_data_df.at[0, 'Date']))
-
-# sort by date
 user_data_df = user_data_df.sort_values(by='Date')
-print(user_data_df)
-
-# group data weekly
 user_data_grouped_weekly_df = user_data_df.groupby(pd.Grouper(freq='W-FRI', key='Date'))[
     'pageviews', 'time_on_page_sec'].sum()
-print(user_data_grouped_weekly_df)
-
-# graph
 user_data_grouped_weekly_df = user_data_grouped_weekly_df.reset_index()
-print(user_data_grouped_weekly_df)
-
-# convert datetime to date
 user_data_grouped_weekly_df['Date'] = user_data_grouped_weekly_df['Date'].dt.date
-print(user_data_grouped_weekly_df)
-
-# create subplots area
 fig = make_subplots(
     rows=2, cols=2,
     shared_xaxes=True,
@@ -50,8 +24,6 @@ fig = make_subplots(
            [{"type": "bar"},
             {"type": "table"}]]
 )
-
-# add time on page
 fig.add_trace(
     go.Bar(x=user_data_grouped_weekly_df['Date'],
            y=user_data_grouped_weekly_df['time_on_page_sec'],
@@ -61,8 +33,6 @@ fig.add_trace(
            ),
     row=1, col=1
 )
-
-# add pageviews bar chart
 fig.add_trace(
     go.Bar(x=user_data_grouped_weekly_df['Date'],
            y=user_data_grouped_weekly_df['pageviews'],
@@ -71,8 +41,6 @@ fig.add_trace(
            name='Page Views (Bar Chart 2)'),
     row=2, col=1
 )
-
-# add table
 fig.add_trace(
     go.Table(
         header=dict(
@@ -86,18 +54,10 @@ fig.add_trace(
     ),
     row=2, col=2
 )
-
-######################################################### 2. (Pi chart) which countries contributing most? (JORDAN)
-
-
-# group data weekly starting saturday
 user_data_grouped_country_df = user_data_df.groupby(pd.Grouper(key='country'))[
     'pageviews', 'time_on_page_sec'].sum()
 user_data_grouped_country_df = user_data_grouped_country_df.reset_index()
-print(user_data_grouped_country_df)
 user_data_grouped_country_df = user_data_grouped_country_df.head(10)
-
-# add Pie trace
 fig.add_trace(
     go.Pie(labels=user_data_grouped_country_df["country"],
            values=user_data_grouped_country_df["pageviews"],
@@ -105,15 +65,18 @@ fig.add_trace(
            ),
     row=1, col=2
 )
-
-# Set theme, margin, and annotation in layout
 fig.update_layout(
     template="plotly_dark",
     margin=dict(r=10, t=25, b=40, l=60),
     annotations=[dict(text="", showarrow=False, xref="paper", yref="paper", x=0, y=0)]
 )
-
-fig.show()
+app.layout = html.Div(children=[
+    html.H1(children='Hadi Data Analyst Test Q1 and Q2 Demo'),
+    dcc.Graph(
+        id='example-graph',
+        figure=fig
+    )
+])
 
 if __name__ == '__main__':
-    app.run_server()
+    app.run_server(debug=True)
